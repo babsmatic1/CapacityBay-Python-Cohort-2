72f09a38098c9c60118be0a1480ac75a23d56bc1
Babatunde Abdullahi <ABDULLAHIBABATUNDE644@GMAIL.COM>
2023-06-14 21:24:53 +0100

    Update function.py

--- Module Importer/function.py
+++ Module Importer/function.py
@@ -1,13 +1,14 @@
 
 def display_file_content(filename):
     try:
-        with open(filename, 'r') as file:
+        with open(data, 'r') as file:
             content = file.read()
-            print(f"File content of {filename}:")
+            print(f"File content of {data}:")
             print(content)
     except FileNotFoundError:
-        print(f"File '{filename}' not found.")
+        print(f"File '{data}' not found.")
     except IOError:
-        print(f"Error reading file '{filename}'.")
-display_file_content('filename.txt')
+        print(f"Error reading file '{data}'.")
+display_file_content('data.txt')
 
+display_file_content('data.txt')
